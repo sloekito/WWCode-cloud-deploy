69d7ec4ce4d8751af6cfccd57742b3491d003b46
sloekito <sheila.loekito@watchguard.com>
2017-10-23 18:06:25 -0700

    test

--- catalog-web/app.py
+++ catalog-web/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, send_from_directory
 from flask_restful import Resource, Api
 from api.catalog import Catalog
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 api = Api(app)
 
